b6b6672675122c383a29a1bf862661218e1ef604
Caio Barilli <caiobarilli@gmail.com>
2021-12-15 00:16:11 -0300

    Allow save orders from Cart Component

--- food-order/src/components/Cart/Cart.js
+++ food-order/src/components/Cart/Cart.js
@@ -1,11 +1,15 @@
-import { useContext } from 'react';
+import { useContext, useState } from "react";
 
-import Modal from '../UI/Modal';
-import CartItem from './CartItem';
-import classes from './Cart.module.css';
-import CartContext from '../../store/cart-context';
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import Checkout from "./Checkout";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setisSubmitting] = useState(false);
+  const [isDidSubmit, setIsDidSubmit] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -19,8 +23,29 @@ const Cart = (props) => {
     cartCtx.addItem(item);
   };
 
+  const orderHandler = (props) => {
+    setIsCheckout(true);
+  };
+
+  const submitOrderHandler = async (userData) => {
+    setisSubmitting(true);
+    await fetch(
+      "https://react-http-23637-default-rtdb.firebaseio.com/orders.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          user: userData,
+          orderItems: cartCtx.items,
+        }),
+      }
+    );
+    setisSubmitting(false);
+    setIsDidSubmit(true);
+    cartCtx.cleanCart();
+  };
+
   const cartItems = (
-    <ul className={classes['cart-items']}>
+    <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
@@ -34,19 +59,41 @@ const Cart = (props) => {
     </ul>
   );
 
-  return (
-    <Modal onClose={props.onClose}>
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
+  const contentModal = (
+    <>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
+      {!isCheckout && modalActions}
+    </>
+  );
+  const modalIsLoading = <p>Loading ... </p>;
+  const modalIsReady = <p>Sucess! </p>;
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !isDidSubmit && contentModal}
+      {isSubmitting && modalIsLoading}
+      {isDidSubmit && modalIsReady}
     </Modal>
   );
 };
