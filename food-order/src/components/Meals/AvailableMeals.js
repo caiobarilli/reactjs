db5fe117e6e5b1abca3fcb6e95de8d4ebcb79d24
Caio Barilli <caiobarilli@gmail.com>
2021-12-15 00:14:20 -0300

    Add reactive feedback and handling errors

--- food-order/src/components/Meals/AvailableMeals.js
+++ food-order/src/components/Meals/AvailableMeals.js
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react";
+
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const [mealt, setMeals] = useState([]);
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(false);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-http-23637-default-rtdb.firebaseio.com/meals.json"
       );
-      const responseData = await response.json();
 
-      console.log(responseData);
+      if (!response) {
+        throw new Error("No response.");
+      }
+
+      const responseData = await response.json();
 
       const loadMeals = [];
 
@@ -27,12 +33,32 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
-  const mealsList = mealt.map((meal) => (
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
