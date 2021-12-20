ea788c63c3f4d38e5b4766f32b4710a3e68ed239
Caio Barilli <caiobarilli@gmail.com>
2021-12-20 10:55:53 -0300

    Add login feature

--- 10-redux/react-app/src/App.js
+++ 10-redux/react-app/src/App.js
@@ -1,9 +1,19 @@
-import Counter from './components/Counter';
-
+import React from "react";
+import { useSelector } from "react-redux";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <React.Fragment>
+      <Header />
+      {isAuth ? <UserProfile /> : <Auth />}
+      <Counter />
+    </React.Fragment>
   );
 }
 
--- 10-redux/react-app/src/components/Auth.js
+++ 10-redux/react-app/src/components/Auth.js
@@ -1,17 +1,31 @@
-import classes from './Auth.module.css';
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/auth";
+import classes from "./Auth.module.css";
 
 const Auth = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const dispatch = useDispatch();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (emailRef.current.value !== "" && passwordRef.current.value !== "") {
+      dispatch(authActions.login());
+    }
+  };
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={submitHandler}>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input ref={emailRef} type="email" id="email" />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input ref={passwordRef} type="password" id="password" />
           </div>
           <button>Login</button>
         </form>
